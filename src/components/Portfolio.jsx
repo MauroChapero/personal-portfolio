2605f114e8421d66bcd325853bc04008dd3da24a
MauroChapero <chaperomauro1803@gmail.com>
2022-12-12 13:25:20 -0300

    Update titulos

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,7 +7,7 @@ function Portfolio() {
   return (
     <div className="flex flex-col md:flex-row justify-center my-20">
       <div className="w-full md:w-7/12">
-        <Title>Projects</Title>
+        <Title>Proyectos</Title>
         <div className="flex flex-col md:flex-row items-center justify-center">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
             {portfolio.map((project) => (
