a5e9176d984f66bc725f2c511f3e3276af533763
MauroChapero <chaperomauro1803@gmail.com>
2023-05-17 19:40:08 -0300

    Update about me info

--- src/components/data/intro-es.js
+++ src/components/data/intro-es.js
@@ -2,5 +2,5 @@ export default {
     firstName: "Mauro",
     lastName: "Chapero",
     carreer: "Desarrollador Web Fullstack",
-    about: "Soy una persona proactiva, responsable, dedicada y empática. Siempre empeño mucha dedicación para la realización de mis labores, comparto mis conocimientos y me comunico de manera efectiva."
+    about: "Comprometido y orientado a resultados, destaco por mi comunicación efectiva actitud proactiva y mentalidad de aprendizaje constante. Disfruto enfrentando desafíos y buscando soluciones innovadoras para mejorar los procesos y la eficiencia. Mi objetivo es crecer utilizando mis habilidades técnicas y socioemocionales para lograr un impacto positivo en el desarrollo de proyectos y en el éxito de los equipos con los que trabajo."
 }
