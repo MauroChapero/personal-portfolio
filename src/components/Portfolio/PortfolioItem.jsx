afb20f7f2e8aef95d819200d3e165beb2cfaacaa
MauroChapero <chaperomauro1803@gmail.com>
2023-05-20 22:13:59 -0300

    Refactor file path

--- src/components/Portfolio/PortfolioItem.jsx
+++ src/components/Portfolio/PortfolioItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../styles/PortfolioItem.css'
+import '../../styles/PortfolioItem.css'
 
 function PortfolioItem({ title, imgUrl, stack, deploy, repository }) {
   return (
